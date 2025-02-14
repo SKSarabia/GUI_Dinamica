52df5b76e769bf18a23724406a9a64aa91375d8d
SKSarabia <al23760342@ite.edu.mx>
2025-02-14 13:08:51 -0800

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_Dinamica
+{
+    public partial class Form1 : Form
+    {
+        private Button btnAddControls;
+        private List<Button> dynamicButtons = new List<Button>();
+        private List<TextBox> dynamicTextBoxes = new List<TextBox>();
+        private int controlCounter = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Configuración de la ventana
+            this.Text = "Creación Dinámica de Controles";
+            this.Size = new Size(400, 400);
+
+            // Botón para agregar controles dinámicamente
+            btnAddControls = new Button();
+            btnAddControls.Text = "Agregar Controles";
+            btnAddControls.Location = new Point(20, 20);
+            btnAddControls.Click += new EventHandler(AddControls);
+            this.Controls.Add(btnAddControls);
+        }
+
+        private void AddControls(object sender, EventArgs e)
+        {
+            // Crear un nuevo botón
+            Button newButton = new Button();
+            newButton.Text = "Botón " + controlCounter;
+            newButton.Size = new Size(100, 30);
+            newButton.Location = new Point(20, 60 + dynamicButtons.Count * 40);
+            newButton.Click += DynamicButtonClick;
+
+            // Crear una nueva caja de texto
+            TextBox newTextBox = new TextBox();
+            newTextBox.Size = new Size(150, 30);
+            newTextBox.Location = new Point(140, 60 + dynamicTextBoxes.Count * 40);
+
+            dynamicTextBoxes.Add(newTextBox);
+            dynamicButtons.Add(newButton);
+
+            this.Controls.Add(newTextBox);
+            this.Controls.Add(newButton);
+
+            controlCounter++;
+        }
+        private void DynamicButtonClick(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            MessageBox.Show("Has presionado: " + clickedButton.Text);
+        }
+
+    }
+}
---
+++ GUI_Dinamica/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace GUI_Dinamica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeFlowLayoutPanel();
+            LoadImages();
+        }
+
+        private void InitializeFlowLayoutPanel()
+        {
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            this.Controls.Add(flowLayoutPanel1);
+        }
+
+        private void LoadImages()
+        {
+            string directoryPath = "D:\\luist\\Documents\\ImagenesDinamicas"; //CAMBIAR EL DIRECTORIO AQUI
+            if (Directory.Exists(directoryPath))
+            {
+                string[] files = Directory.GetFiles(directoryPath, "*.jpg"); //CAMBIAR EL TIPO DE ARCHIVO AQUI
+                foreach (string file in files)
+                {
+                    PictureBox picBox = new PictureBox();
+                    picBox.Image = Image.FromFile(file);
+                    picBox.SizeMode = PictureBoxSizeMode.Zoom;
+                    picBox.Width = 100;
+                    picBox.Height = 100;
+                    picBox.Click += (sender, e) => ShowImageInNewWindow(file);
+                    flowLayoutPanel1.Controls.Add(picBox);
+                }
+            }
+            else
+            {
+                MessageBox.Show("El directorio no existe");
+            }
+        }
+
+        private void ShowImageInNewWindow(string filePath)
+        {
+            Form imageForm = new Form();
+            imageForm.Text = Path.GetFileName(filePath);
+            PictureBox picBox = new PictureBox();
+            picBox.Image = Image.FromFile(filePath);
+            picBox.Dock = DockStyle.Fill;
+            picBox.SizeMode = PictureBoxSizeMode.Zoom;
+            imageForm.Controls.Add(picBox);
+            imageForm.ShowDialog();
+        }
+    }
+
+}
